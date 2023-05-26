c0dd641056a3ff13fa093741731a5892e869d74f
Pavel Shlapak <letohx@gmail.com>
2023-05-27 01:10:53 +0300

    feat: add clock and calendar

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Сlock and Сalendar
+
+const time = document.querySelector('.time');
+const dateString = document.querySelector('.date');
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  time.textContent = currentTime;
+  setTimeout(showTime, 1000);
+  showDate();
+};
+showTime();
+
+function showDate() {
+  const date = new Date();
+  const options = {weekday: 'long', month: 'long', day: 'numeric'};
+  const currentDate = date.toLocaleDateString('en-US', options);
+  dateString.textContent = currentDate;
+};
