30b7c957331b1d9a57cd8dd8af749e840c4d1842
Pavel Shlapak <letohx@gmail.com>
2023-05-31 01:06:22 +0300

    feat: add audio player

---
+++ js/playList.js
@@ -0,0 +1,31 @@
+const playList = [
+    {      
+      title: 'River Flows In You',
+      src: './assets/sounds/River Flows In You.mp3',
+      duration: '03:50'
+    },
+    {      
+      title: 'Interstellar Theme',
+      src: './assets/sounds/Interstellar Theme.mp3',
+      duration: '05:05'
+    },
+    {       
+      title: 'Aqua Caelestis',
+      src: './assets/sounds/Aqua Caelestis.mp3',
+      duration: '00:58'
+    },
+    {      
+      title: 'Purring Cat',
+      src: 'https://7oom.ru/audio/cat.mp3',
+      duration: '03:50'
+    },
+    {      
+      title: 'Birdsong',
+      src: 'https://7oom.ru/audio/naturesounds/07%20Birds%20(7oom.ru).mp3',
+      duration: '00:58'
+    }
+
+
+
+]
+export default playList;
